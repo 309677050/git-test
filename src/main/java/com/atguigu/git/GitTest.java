6cf1e17502cc3c4bde8a9810bebe93b11236a83a
zhaoyue <309677050@qq.com>
2021-07-23 19:06:59 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zy
+ * @create 2021-07-23 19:03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
